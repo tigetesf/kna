5df1d1203757a3d74c8f51087fccc8d906e35458
tigetesf <164871030+tigetesf@users.noreply.github.com>
2024-04-04 06:01:33 -0700

    Add files via upload

---
+++ knap.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>kena web</title>
+    <link rel="stylesheet" href="knap.css">
+</head>
+<body>
+ <div class="main">
+    <div class="navbar">
+<div class="icon">
+
+     
+    </div>
+    <div class="menu">
+    
+     <ul>
+        <li> <a href="knar.php">about</a></li>
+        <li> <a href="index.php">registration</a></li>
+        <li> <a href="disign">disign</a></li>
+     </ul>
+     </div>
+     </div>
+     <img src="tg.jpg" alt="KENNA Logo">
+     <div class="cont">
+     <h1> KENA <br> <span>GARGAARSA</span> <br>ORGANIZATION</h1>
+     <p class="para"> Reasonable Faith is intended primarily to serve as a textbook for seminary level 
+courses on Christian apologetics. Indeed, the book began </p>
+       <button class="learn"><a href="index.php">startnow</a> </button>    </div>
+
+     </div>
+    
+    
+</body>
+</html>
---
+++ knar.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="knar.css">
+</head>
+<body>
+    <div class="main">
+    <button class="learn"><a href="knap.php">previous</a> </button> 
+<div class="hed">
+<h1>ORGANIZATION INFORMATION</h1>
+</div>
+<div class="par1">
+    <p>
+“It is hard to overstate the impact that William Lane Craig has had for the 
+cause of Christ. He is simply the finest Christian apologist of the last half 
+century, and his academic work justifies ranking him among the top 1 percent 
+of practicing philosophers in the Western world. Besides that, he is a winsome 
+ambassador for Christ, an exceptional debater, and a man with the heart of 
+an evangelist. I know him well and can say that he lives a life of integrity and 
+lives out what he believes. I do not know of a single thinker who has done 
+more to raise the bar of Christian scholarship in our generation than Craig. 
+He is one of a kind, and I thank God for his life and work.”   
+    </p>
+</div>
+<div class="par2">
+<p>
+Organization information fields go here...
+
+
+“It is hard to overstate the impact that William Lane Craig has had for the 
+cause of Christ. He is simply the finest Christian apologist of the last half 
+century, and his academic work justifies ranking him among the top 1 percent 
+of practicing philosophers in the Western world. Besides that, he is a winsome 
+ambassador for Christ, an exceptional debater, and a man with the heart of 
+an evangelist. I know him well and can say that he lives a life of integrity and 
+lives out what he believes. I do not know of a single thinker who has done 
+more to raise the bar of Christian scholarship in our generation than Craig. 
+He is one of a kind, and I thank God for his life and work.”
+</p>
+</div>
+    </div>
+    
+</body>
+</html>
